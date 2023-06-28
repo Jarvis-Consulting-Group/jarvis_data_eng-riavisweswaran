758447e6bb3eeec3f7d59273f80799ac566eef5b
Ria Visweswaran <riavisweswaran1@gmail.com>
2023-06-28 20:22:06 +0000

    spacing

--- sql/queries.sql
+++ sql/queries.sql
@@ -51,22 +51,26 @@ INSERT INTO cd.facilities (facid, name, membercost, guestcost, initialoutlay, mo
 
 -- Question 3
 -- We made a mistake when entering the data for the second tennis court. The initial outlay was 10000 rather than 8000: you need to alter the data to fix the error.
+
 UPDATE cd.facilities
 SET initialoutlay=10000
 WHERE name='Tennis Court 2';
 
 -- Question 4
 -- We want to alter the price of the second tennis court so that it costs 10% more than the first one. Try to do this without using constant values for the prices, so that we can reuse the statement if we want to.
+
 UPDATE cd.facilities
 SET membercost = membercost * 1.1, guestcost = guestcost * 1.1
 WHERE facid = 1;
 
 -- Question 5
 -- As part of a clearout of our database, we want to delete all bookings from the cd.bookings table. How can we accomplish this?
+
 DELETE FROM cd.bookings;
 
 --Question 6
 --We want to remove member 37, who has never made a booking, from our database. How can we achieve that?
+
 DELETE FROM  cd.members
 WHERE memid = 37;
 
